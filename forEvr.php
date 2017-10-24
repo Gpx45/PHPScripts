1a8eb8e3a49c6e49d444fc502f0352026b0925db
Victor Martins <vich.martins@gmail.com>
2017-10-24 08:16:34 -0400

    added

---
+++ forEvr.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$FastFood = array(" hot dog "
+,"hamburger"," anal fudge "," poon "," cigarrettes ");
+
+for ($Count = 0; $Count < 5; $Count++){
+    $FastFood[$Count];
+    echo "$FastFood[$Count] <br />";
+}
+?>
