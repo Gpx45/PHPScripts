732c2a7304b1fcae796d0300abdbbc189c23f5d3
Victor Martins <vich.martins@gmail.com>
2017-10-24 11:13:06 -0400

    added

--- forEvr.php
+++ forEvr.php
@@ -8,4 +8,22 @@ for ($Count = 0; $Count < 5; $Count++){
     $FastFood[$Count];
     echo "$FastFood[$Count] <br />";
 }
+
+// Foreach
+
+$DaysOfTheWeek = array("Monday","Tuesday","Wednesday",
+"Thursday","Friday","Saturday","Sunday");
+
+foreach ($DaysOfTheWeek as $Days){
+    echo " <br />$Days";
+}
+// A more advanced method that can retruves both value and
+// array element of the array.
+
+foreach ($DaysOfTheWeek as $x => $y ){
+    echo "$x on $y <br />";
+// $x is the index location
+// $y is the value in that index
+
+}
 ?>
